33323b0f4b9f85aa20beb1eb2e701b495e0988a1
Abhineet Raj <abhineetraj@hotmail.com>
2020-05-05 09:32:08 -0700

    Update test_redis.py

--- test_redis.py
+++ test_redis.py
@@ -1,9 +1,9 @@
 import redis
 
 r = redis.Redis(
-    host='ms5stage-redis-001.mpulse.io',
+    host='google-redis-001.mpulse.io',
     port=6379, 
-    db=5)
+    db=6)
 
 r.set('foo', 'bar')
 value = r.get('foo')
